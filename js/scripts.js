6024781eb29acbf53312bff51d32affd649ffba0
Aiden Kwok <aidenkwok96@gmail.com>
2017-11-13 13:19:01 +0000

    new site with more vids

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,47 @@
 $(document).ready(function(){
-  setInterval(function () {
-      var date = new Date();
-      var time = date.getHours() + ":" + date.getMinutes();
-      if (
-        date.getHours() === 09 ||
-        date.getHours() === 10 ||
-        date.getHours() === 11 ||
-        date.getHours() === 12 ||
-        date.getHours() === 13 ||
-        date.getHours() === 14 ||
-        date.getHours() === 15 ||
-        date.getHours() === 16 ||
-        date.getHours() === 17 ||
-        date.getHours() === 18 ||
-        date.getHours() === 19 ||
-        date.getHours() === 20 ||
-        date.getHours() === 21 ||
-        date.getHours() === 22 ||
-        date.getHours() === 23 ||
-        date.getHours() === 00
-    ) {
-      $(".video").replaceWith(
-        '<iframe src="https://www.youtube.com/embed/bfUBz0-jjg4?rel=0&amp;controls=0&amp;showinfo=0?rel=0&autoplay=1&loop=1" frameborder="0" allowfullscreen style="position: relative; height: 100%; width: 100%;"></iframe>'
-      )
-    }
-  }, 3000)
+  var date = new Date();
+  var time = date.getHours() + ":" + date.getMinutes();
+  var iframe1 =  '<iframe src="https://www.youtube.com/embed/';
+  var iframe2= '?rel=0&amp;controls=0&amp;showinfo=0?rel=0&autoplay=1&loop=1" frameborder="0" allowfullscreen style="position: relative; height: 100%; width: 100%;"></iframe>';
 
-});
+  setInterval(function () {
+
+
+      //YOUTUBE
+      if (date.getHours() === 0) {
+        $(".video").replaceWith(iframe1 + "uM5CjB5QZ8s" +iframe2);
+
+      // ABOUT TO GO TO SLEEP
+      }else if(date.getHours() < 0 && date.getHours() >2  ){
+        $(".video").replaceWith(iframe1 + "PlJnqCtDqO8" +iframe2);
+
+
+      // SLEEP
+    }else if(date.getHours() > 1 && date.getHours() < 8 ){
+      $(".video").replaceWith(iframe1 + "kRUH9dphNmI" +iframe2);
+
+
+      // CHECKING SOCIAL MEDIA
+    }else if(date.getHours() === 08){
+      $(".video").replaceWith(iframe1 + "k-XpvvJC9FU" +iframe2);
+
+
+      // AVALIBLE
+    }else if(date.getHours() >8 &&  date.getHours() <13 ||  date.getHours() >13 && date.getHours() <18){
+        $(".video").replaceWith(iframe1 + "lEzgTp7GNz0" + iframe2);
+
+      // LUNCH
+    }else if(date.getHours() === 13){
+        $(".video").replaceWith(iframe1 + "_OXwJ2qhSTQ" +iframe2);
+
+      // DO NO DISTURB
+    }else if(date.getHours() >17 && date.getHours() < 20 || date.getHours() >20 && date.getHours() < 24){
+        $(".video").replaceWith(iframe1 + "ZpIN7ENkn_Y" +iframe2);
+
+      // DINNER
+    }else if(date.getHours() === 20){
+      $(".video").replaceWith(iframe1 + "7gn6OR2e-iY" +iframe2);
+      }
+    }, 2000);
+
+  });
